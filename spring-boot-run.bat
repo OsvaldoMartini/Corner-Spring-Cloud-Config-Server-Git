9932a37f9a40c45518a1cb183068555e09b80f69
Osvaldo Martini <maos@corner.ch>
2019-11-22 14:20:52 +0100

    Update spring-boot-run.bat

--- spring-boot-run.bat
+++ spring-boot-run.bat
@@ -6,10 +6,10 @@ SETLOCAL
 set JAVA_HOME=C:\Java Tools\jdk1.8.0_144
 set path=C:\Java Tools\jdk1.8.0_144\bin;%path%
 
-set MAVE_HOME=C:\Appl\apache-maven-3.5.2
+set MAVE_HOME=C:\Java Tools\apache-maven-3.5.2
 set path=%MAVE_HOME%\bin;%path%
 
-set M2_HOME=C:\Appl\apache-maven-3.5.2
+set M2_HOME=C:\Java Tools\apache-maven-3.5.2
 set path=%M2_HOME%\bin;%path%
 
 
